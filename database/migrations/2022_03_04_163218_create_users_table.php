7b3f05d4092849409d139f98b29ce23b52150bdc
Sooyz <47371084+Sooyz@users.noreply.github.com>
2022-03-04 13:51:32 -0300

    Initial commit

---
+++ database/migrations/2022_03_04_163218_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cpf');
+            $table->string('user')->unique();
+            $table->date('dt_born');
+            $table->enum('profile', array('admin', 'influencer'));
+            $table->foreignId('social_id')->constrained('social_networks');
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
